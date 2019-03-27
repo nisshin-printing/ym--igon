f4d2bbb3665b6361624ef7d4763a5a298f56077e
kazuki-nisshin <nisshin@dtdsh.com>
2019-03-27 17:51:24 +0900

    画像の追加

--- theme/front-page.php
+++ theme/front-page.php
@@ -12,11 +12,6 @@
 		<p class="text-center"><img src="<?php echo get_template_directory_uri(), '/assets/img/souzokutop.png'; ?>" alt="広島で相続・遺言のご相談なら山下江法律事務所へ！"></p>
 
 
-		<?php get_template_part( './elements/fp-welcome-mess' ); ?>
-
-
-		<?php // get_template_part( 'elements/cta--jiko-lp' ); ?>
-
 
 
 		<div class="row">
@@ -25,70 +20,127 @@
 			</div>
 			<div class="sidenav--overlay js--sidenav--button"></div>
 			<div class="column small-12 large-8 contents --mt0 --pt0">
-				<section class="contents--section contents--section__pickup">
-					<div class="row text-center">
-						<div class="pickup--background column small-12 medium-6">
-							<h3><img src="<?php echo get_template_directory_uri(), '/assets/img/TtlSDTop2_1.jpg'; ?>" alt="相続の基礎知識"></h3>
-							<ul class="pickup--list text-left">
-								<li><a href="<?php echo get_page_link( '6417' ); ?>"><?php echo get_the_title( '6417' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6419' ); ?>"><?php echo get_the_title( '6419' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6438' ); ?>"><?php echo get_the_title( '6438' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6427' ); ?>"><?php echo get_the_title( '6427' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6422' ); ?>"><?php echo get_the_title( '6422' ); ?></a></li>
-							</ul>
-						</div>
-						<div class="pickup--background column small-12 medium-6">
-							<h3><img src="<?php echo get_template_directory_uri(), '/assets/img/TtlSDTop2_2.jpg'; ?>" alt="遺産分割協議"></h3>
-							<ul class="pickup--list text-left">
-								<li><a href="<?php echo get_page_link( '6436' ); ?>"><?php echo get_the_title( '6436' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6427' ); ?>"><?php echo get_the_title( '6427' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6422' ); ?>"><?php echo get_the_title( '6422' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6458' ); ?>"><?php echo get_the_title( '6458' ); ?></a></li>
-							</ul>
-						</div>
-						<div class="pickup--background column small-12 medium-6">
-							<h3><img src="<?php echo get_template_directory_uri(), '/assets/img/TtlSDTop2_3.jpg'; ?>" alt="遺留分・寄与分"></h3>
-							<ul class="pickup--list text-left">
-								<li><a href="<?php echo get_page_link( '6467' ); ?>"><?php echo get_the_title( '6467' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6469' ); ?>"><?php echo get_the_title( '6469' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6473' ); ?>"><?php echo get_the_title( '6473' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6475' ); ?>"><?php echo get_the_title( '6475' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6484' ); ?>"><?php echo get_the_title( '6484' ); ?></a></li>
-							</ul>
-						</div>
-						<div class="pickup--background column small-12 medium-6">
-							<h3><img src="<?php echo get_template_directory_uri(), '/assets/img/TtlSDTop2_4.jpg'; ?>" alt="遺言書"></h3>
-							<ul class="pickup--list text-left">
-								<li><a href="<?php echo get_page_link( '6523' ); ?>"><?php echo get_the_title( '6523' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6533' ); ?>"><?php echo get_the_title( '6533' ); ?></a></li>
-								<li><a href="<?php echo get_page_link( '6535' ); ?>"><?php echo get_the_title( '6535' ); ?></a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="row">
-						<div class="column small-4 text-center"><a href="<?php echo get_page_link( '6070' ); ?>"><img src="<?php echo get_template_directory_uri(), '/assets/img/ImgTop2.jpg';?>" alt="<?php echo get_the_title( '6070' ); ?>"></a></div>
-						<div class="column small-4 text-center"><a href="<?php echo get_page_link( '5487' ); ?>"><img src="<?php echo get_template_directory_uri(), '/assets/img/ImgTop3.jpg';?>" alt="<?php echo get_the_title( '5487' ); ?>"></a></div>
-						<div class="column small-4 text-center"><a href="<?php echo get_page_link( '5492' ); ?>"><img src="<?php echo get_template_directory_uri(), '/assets/img/ImgTop4.jpg';?>" alt="<?php echo get_the_title( '5492' ); ?>"></a></div>
-					</div>
-				</section>
 
 
-				<section class="contents--section contents--section__point">
-					<h2>山下江法律事務所の相続の３つのポイント</h2>
-					<h3>メリット1 相続手続の専門家が直接お話をお伺いします。</h3>
-					<p>当事務所では、相続手続きのご相談において、専門的な知識を備えたNPO法人相続アドバイザー協議会認定の相続アドバイザー・上級アドバイザーが承ります。</p>
-					<h3>メリット2  相続手続から、相続争いまで、すべてに対応できます。</h3>
-					<p>「争 いはないけど、複雑な相続手続をどう進めて良いか分からない」「自分がしなければならない手続きがどれか分からない」という相談から、「親族ともめてし まったので解決したい」という相談まで、相続に関することなら何でも対応することができます。所内には弁護士の他、相続アドバイザー・上級アドバイザーが 在籍しており、税理士、司法書士、行政書士などとも連携をしていますので、速やかに相続手続きも行うことができます。</p>
-					<h3>メリット3 広島最大級の法律事務所</h3>
-					<p>当事務所は士業事務所に良く見られる個人事務所ではなく、所員総勢38名の広島最大級の法律事務所です。総合力と機動力は地域ナンバーワンと自負しております。実績多数の当法律事務所に、是非ご相談ください。</p>
-					<h3>相続アドバイザー・上級アドバイザーによる相談も、無料で承っています</h3>
-					<p>・弁護士に相談するのは敷居が高い<br>・ちょっとだけ教えてもらいたいことがある<br>と思われる方は、相続アドバイザーにお気軽にお電話ください。</p>
+
+			<section class="contents--section contents--section__pickup">
+					<h2 style="margin-top:1rem;border-left:none;padding-left:0"><img class="lazyload" data-src="//tokyo.law-yamashita.com/wp-content/themes/ym--tokyo/assets/img/top--reason__title.png" alt="山下江法律事務所が相続・遺言で選ばれる５つの理由"></h2>
+					<h3>弊所の弁護士と相続アドバイザーが<br>広島初の一般向け『相続本』を出版</h3>
+					<div class="row align-middle">
+						<div class="column small-3"><img class="lazyload" alt="山下江法律事務所の弁護士と相続アドバイザーが執筆。相続・遺言のポイント50" data-src="//tokyo.law-yamashita.com/wp-content/themes/ym--tokyo/assets/img/sozoku-book--50.png"></div>
+						<div class="column small-9">
+							<p>弁護士と相続アドバイザーが執筆した、一般向けの相続解説本を2016年に出版しました。</p>
+							<p>無用なトラブルを避けるためにも「<strong class="underline">一家に一冊相続本を</strong>」との思いで、山下江法律事務所の弁護士と相続アドバイザーが執筆しました。法律問題・税務問題についてイラストを多用して、最もわかりやすいものを目指しました。</p>
+						</div>
+					</div>
+					<h3>広島最大級の解決実績</h3>
+					<div class="row align-middle">
+						<div class="column small-4"><img alt="広島最大級の解決事例を誇る山下江法律事務所の相談件数" class="lazyload" data-src="//tokyo.law-yamashita.com/wp-content/themes/ym--tokyo/assets/img/sodan--2018.png"></div>
+						<div class="column small-4"><img alt="広島最大級の解決事例を誇る山下江法律事務所の受任件数" class="lazyload" data-src="//tokyo.law-yamashita.com/wp-content/themes/ym--tokyo/assets/img/jyunin--2018.png"></div>
+						<div class="column small-4"><img alt="広島最大級の解決事例を誇る山下江法律事務所の満足度" class="lazyload" data-src="//tokyo.law-yamashita.com/wp-content/themes/ym--tokyo/assets/img/manzokudo.png"></div>
+					</div>
+					<p>地元広島における弁護士事務所で、これだけ多数の相談件数・受任件数・お客様アンケート満足度を有しているのは当事務所のみと思われます。</p>
+					<h3><strong class="underline">相続・遺言問題に精通した弁護士や相続アドバイザー</strong>が直接お話をお伺いします。</h3>
+					<p>専門的知識を備えたNPO法人相続アドバイザー協会認定の相続アドバイザー・上級相続アドバイザーが承ります。</p>
+					<h3>弁護士による初回相談<strong class="underline">無料</strong><br>相続アドバイザーによる相談<strong class="underline">無料</strong></h3>
+					<ul>
+						<li>弁護士へ相談するのは敷居が高い</li>
+						<li>ちょっとだけ教えてほしいことがある</li>
+					</ul>
+					<p>と思われる方が相談しやすいよう相続アドバイザーへの相談が無料です。<br>また弁護士による初回相談も無料となっております。</p>
 					<p class="text-center"><img src="<?php echo get_template_directory_uri(), '/assets/img/BgSDTop4.png'; ?>" alt="相続アドバイザーが丁寧にお答えします！"><br>◆チェックリストはこちら☞<a href="<?php echo get_page_link( '6405' ); ?>">相続対策チェック</a></p>
-					<p class="text-center"><a href="https://www.law-yamashita.com/contact" target="_blank"><img src="<?php echo get_template_directory_uri(), '/assets/img/banner_01.png'; ?>" alt="広島の相続・遺言相談は縮景園駅すぐの山下江法律事務所へ"></a></p>
+					<h3>相続法大改正に対応して<br>改訂版『相続・遺言のポイント５５』を発刊</h3>
+					<div class="row align-middle">
+						<div class="column small-3"><img class="lazyload" alt="山下江法律事務所の弁護士と相続アドバイザーが執筆。相続・遺言のポイント55" data-src="//tokyo.law-yamashita.com/wp-content/themes/ym--tokyo/assets/img/sozoku-book--55.png"></div>
+						<div class="column small-9">
+							<p>一般向けの相続解説本を2016年に出版しました。<br>この度、相続法大改正に対応して、改訂版『相続・遺言のポイント５５』を発刊します。</p>
+							<p><img alt="相続本はこんな疑問にお答えします。" class="lazyload" data-src="//tokyo.law-yamashita.com/wp-content/themes/ym--tokyo/assets/img/sozoku-book--55__answer.png"></p>
+							<ul>
+								<li>「改正で遺言が書きやすくなったとは？」</li>
+								<li>「改正により配偶者の保護が強化されたとは？」</li>
+								<li>「相続手続きには期限があるってホント！？」</li>
+								<li>「遺言書が２通あった！どうすればいい？」</li>
+								<li>「相続税、うちに関係あるの？」</li>
+							</ul>
+						</div>
+					</div>
+					<h3>終活のご相談から手続、相続争いまで、すべてに対応できます。</h3>
+					<p>「争いはないけど、複雑な相続手続をどう進めて良いか分からない」「自分がしなければならない手続きがどれか分からない」という相談から、「親族ともめてし まったので解決したい」という相談まで、相続に関することなら何でも対応することができます。所内には弁護士の他、相続アドバイザー・上級アドバイザーが 在籍しており、税理士、司法書士、行政書士などとも連携をしていますので、速やかに相続手続きも行うことができます。</p>
+					<h3>広島近郊の相続・遺言問題を<br>東京にいながら相談可能！</h3>
+					<ul>
+						<li>広島の実家で父親と同居していた兄弟が、遺産分割に応じようとしない。</li>
+						<li>広島にいる親の相続対策を考えておきたい。</li>
+						<li>遺産分割しないまま長年経過してしまった広島にある不動産をどうにかしたい。</li>
+					</ul>
+					<p>広島の法律事務所として東京に支部を持つ事務所は<u>山下江法律事務所が初めて</u>です。<strong class="underline">東京在住の方の相続相談で、対象物件が広島にあるので広島の弁護士に依頼したい</strong>という期待に応えるような法律事務所が広島には今までありませんでした。</p>
 				</section>
 
 
-				<?php get_template_part( './elements/fp--access' ); ?>
+
+
+<section class="contents--section contents--section__pickup">
+	<h2 style="margin-top:1rem">遺言についてお悩みの方に<br>お役立ちコンテンツ</h2>
+	<?php
+		require_once( get_template_directory() . '/config/pickup.php' );
+		foreach ( $pickup as $item ) :
+	?>
+	<div class="row align-middle">
+		<div class="column small-3"><img alt="<?php echo get_the_title( $item['id'] ); ?>" class="lazyload thumbnail" data-src="<?php echo get_template_directory_uri(), '/assets/img/pickup--', $item['image'], '.jpg'; ?>"></div>
+		<div class="column small-9">
+			<h3><a href="<?php echo get_page_link( $item['id'] ); ?>"><?php echo get_the_title( $item['id'] ); ?></a></h3>
+			<?php echo $item['desc']; ?>
+			<p class="text-center"><a href="<?php echo get_page_link( $item['id'] ); ?>" class="button"><?php echo get_the_title( $item['id'] ); ?></a></p>
+		</div>
+	</div>
+	<?php endforeach; ?>
+</section>
+
+
+
+
+<section class="contents--section contents--section__pickup">
+	<h2 style="margin-top:1rem">相続の基礎知識</h2>
+	<div class="row text-center">
+		<div class="pickup--background column small-12 medium-6">
+			<h3><img src="//hiroshima-sozoku.com/wp-content/themes/ym--sozoku/assets/img/TtlSDTop2_1.jpg" alt="相続の基礎知識"></h3>
+			<ul class="pickup--list text-left">
+				<li><a href="<?php echo get_page_link( '6417' ); ?>"><?php echo get_the_title( '6417' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6419' ); ?>"><?php echo get_the_title( '6419' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6438' ); ?>"><?php echo get_the_title( '6438' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6427' ); ?>"><?php echo get_the_title( '6427' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6422' ); ?>"><?php echo get_the_title( '6422' ); ?></a></li>
+			</ul>
+		</div>
+		<div class="pickup--background column small-12 medium-6">
+			<h3><img src="//hiroshima-sozoku.com/wp-content/themes/ym--sozoku/assets/img/TtlSDTop2_2.jpg" alt="遺産分割協議"></h3>
+			<ul class="pickup--list text-left">
+				<li><a href="<?php echo get_page_link( '6436' ); ?>"><?php echo get_the_title( '6436' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6427' ); ?>"><?php echo get_the_title( '6427' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6422' ); ?>"><?php echo get_the_title( '6422' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6458' ); ?>"><?php echo get_the_title( '6458' ); ?></a></li>
+			</ul>
+		</div>
+		<div class="pickup--background column small-12 medium-6">
+			<h3><img src="//hiroshima-sozoku.com/wp-content/themes/ym--sozoku/assets/img/TtlSDTop2_3.jpg" alt="遺留分・寄与分"></h3>
+			<ul class="pickup--list text-left">
+				<li><a href="<?php echo get_page_link( '6467' ); ?>"><?php echo get_the_title( '6467' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6469' ); ?>"><?php echo get_the_title( '6469' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6473' ); ?>"><?php echo get_the_title( '6473' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6475' ); ?>"><?php echo get_the_title( '6475' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6484' ); ?>"><?php echo get_the_title( '6484' ); ?></a></li>
+			</ul>
+		</div>
+		<div class="pickup--background column small-12 medium-6">
+			<h3><img src="//hiroshima-sozoku.com/wp-content/themes/ym--sozoku/assets/img/TtlSDTop2_4.jpg" alt="遺言書"></h3>
+			<ul class="pickup--list text-left">
+				<li><a href="<?php echo get_page_link( '6523' ); ?>"><?php echo get_the_title( '6523' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6533' ); ?>"><?php echo get_the_title( '6533' ); ?></a></li>
+				<li><a href="<?php echo get_page_link( '6535' ); ?>"><?php echo get_the_title( '6535' ); ?></a></li>
+			</ul>
+		</div>
+	</div>
+</section>
+
 
 
 
@@ -99,7 +151,7 @@
 				);
 				$news = new WP_Query( $args );
 				if ( $news->have_posts() ) :
-			?>
+					?>
 					<div class="contents--section">
 						<h2>
 							<a href="<?php echo get_page_link( '5749' ); ?>">最新情報
@@ -108,7 +160,7 @@
 						</h2>
 						<?php
 					while ( $news->have_posts() ) : $news->the_post();
-				?>
+					?>
 							<dl>
 								<dt>
 									<span class="label secondary">
@@ -124,7 +176,7 @@
 							</dl>
 							<?php
 					endwhile;
-				?>
+					?>
 							<p class="text-center contents--section_button" style="margin-top: 2rem">
 								<a href="<?php echo get_page_link( '5749' ); ?>" class="button large">
 									<?php NID_SVG::icon( 'list', array() ); ?>お知らせ一覧</a>
@@ -153,7 +205,7 @@
 					}
 					$site_title = $feed->get_title();
 					$site_url = $feed->get_permalink();
-
+					
 					$output = '';
 					date_default_timezone_set( 'Asia/Tokyo' );
 					foreach ( $rss_items as $item ) {
@@ -185,5 +237,7 @@ EOM;
 
 			</div>
 		</div>
+		<?php get_template_part( './elements/fp-welcome-mess' ); ?>
+		<?php get_template_part( './elements/fp--access' ); ?>
 	</main>
 	<?php get_footer(); ?>
--- theme/index.php
+++ theme/index.php
@@ -62,10 +62,201 @@ if ( is_archive() || is_single() || is_home() ) {
 						$show_cat = '313';
 						include locate_template( './templates/page--column.php' );
 
-					} else if ( is_singular( 'dtdsh-lp' ) ) {
-						if ( is_single( 'jiko' ) ) {
-							get_template_part( './templates/lp/jiko' );
-						}
+
+
+
+
+
+
+					/**
+					 * 相続の基礎知識
+					 */
+				} else if ( is_page( '6373' ) ) {
+					$isSozoku = true;
+					$page_id = '6373';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6560' ) ) {
+					$isSozoku = true;
+					$page_id = '6560';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6567' ) ) {
+					$isSozoku = true;
+					$page_id = '6567';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6713' ) ) {
+					$isSozoku = true;
+					$page_id = '6713';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6562' ) ) {
+					$isSozoku = true;
+					$page_id = '6562';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6414' ) ) {
+					$isSozoku = true;
+					$page_id = '6414';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6417' ) ) {
+					$isSozoku = true;
+					$page_id = '6417';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6419' ) ) {
+					$isSozoku = true;
+					$page_id = '6419';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6718' ) ) {
+					$isSozoku = true;
+					$page_id = '6718';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6427' ) ) {
+					$isSozoku = true;
+					$page_id = '6427';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6422' ) ) {
+					$isSozoku = true;
+					$page_id = '6422';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6409' ) ) {
+					$isSozoku = true;
+					$page_id = '6409';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6539' ) ) {
+					$isSozoku = true;
+					$page_id = '6539';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6541' ) ) {
+					$isSozoku = true;
+					$page_id = '6541';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6543' ) ) {
+					$isSozoku = true;
+					$page_id = '6543';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6547' ) ) {
+					$isSozoku = true;
+					$page_id = '6547';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6545' ) ) {
+					$isSozoku = true;
+					$page_id = '6545';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6484' ) ) {
+					$isSozoku = true;
+					$page_id = '6484';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6488' ) ) {
+					$isSozoku = true;
+					$page_id = '6488';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6486' ) ) {
+					$isSozoku = true;
+					$page_id = '6486';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6717' ) ) {
+					$isSozoku = true;
+					$page_id = '6717';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6446' ) ) {
+					$isSozoku = true;
+					$page_id = '6446';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6438' ) ) {
+					$isSozoku = true;
+					$page_id = '6438';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6440' ) ) {
+					$isSozoku = true;
+					$page_id = '6440';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6496' ) ) {
+					$isSozoku = true;
+					$page_id = '6496';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6500' ) ) {
+					$isSozoku = true;
+					$page_id = '6500';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6498' ) ) {
+					$isSozoku = true;
+					$page_id = '6498';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6593' ) ) {
+					$isSozoku = true;
+					$page_id = '6593';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6507' ) ) {
+					$isSozoku = true;
+					$page_id = '6507';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6514' ) ) {
+					$isSozoku = true;
+					$page_id = '6514';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6511' ) ) {
+					$isSozoku = true;
+					$page_id = '6511';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6509' ) ) {
+					$isSozoku = true;
+					$page_id = '6509';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6436' ) ) {
+					$isSozoku = true;
+					$page_id = '6436';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6460' ) ) {
+					$isSozoku = true;
+					$page_id = '6460';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6454' ) ) {
+					$isSozoku = true;
+					$page_id = '6454';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6458' ) ) {
+					$isSozoku = true;
+					$page_id = '6458';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6456' ) ) {
+					$isSozoku = true;
+					$page_id = '6456';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6467' ) ) {
+					$isSozoku = true;
+					$page_id = '6467';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6473' ) ) {
+					$isSozoku = true;
+					$page_id = '6473';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6469' ) ) {
+					$isSozoku = true;
+					$page_id = '6469';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6475' ) ) {
+					$isSozoku = true;
+					$page_id = '6475';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6523' ) ) {
+					$isSozoku = true;
+					$page_id = '6523';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6525' ) ) {
+					$isSozoku = true;
+					$page_id = '6525';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6527' ) ) {
+					$isSozoku = true;
+					$page_id = '6527';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6535' ) ) {
+					$isSozoku = true;
+					$page_id = '6535';
+					include locate_template( './templates/page.php' );
+				} else if ( is_page( '6533' ) ) {
+					$isSozoku = true;
+					$page_id = '6533';
+					include locate_template( './templates/page.php' );
+
+
 
 					} else if ( is_page() ) {
 						while ( have_posts() ) : the_post();
